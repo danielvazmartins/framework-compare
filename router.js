33a57e483dda20f0a3136fcd51cd697f022ef5b8
Daniel Martins <daniel.martins@viewit.com.br>
2017-12-05 20:01:01 -0200

    ajustes

--- router.js
+++ router.js
@@ -15,6 +15,12 @@ module.exports = function(app) {
 
 	// Pagina com Angular4
 
+	app.use(function (req, res, next) {
+    	// Website you wish to allow to connect
+    	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    	next();
+	});
+
 	// API de dados
 	var notifications = require('./routes/api/notifications');
 	app.use('/api/notifications', notifications);
